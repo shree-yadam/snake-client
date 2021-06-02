c8a5e95da11758a9e22995abecb7d1316d64bf77
Shree Devi Yadam <shree.yadam@gmail.com>
2021-06-02 18:13:53 +0000

    added sending set messages to server

--- input.js
+++ input.js
@@ -5,14 +5,16 @@ const movementCommand = {
   w: "Move: up",
   a: "Move: left",
   s: "Move: down",
-  d: "Move: right"
+  d: "Move: right",
+  m: "Say: Yay! I can do this",
+  p: "Say: Hmmm......"
 };
 
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     console.log("Quitting");
     process.exit();
-  } else if (key === 'w' || key === 'a' || key === 's' || key === 'd') {
+  } else if (key in movementCommand) {
     connection.write(movementCommand[key]);
   }
 };
@@ -23,6 +25,11 @@ const setupInput = function(conn) {
   stdin.setEncoding("utf8");
   stdin.resume();
   connection = conn;
+  console.log("Use any of the following keys: ");
+  for (let key in movementCommand) {
+    console.log(`${key} - ${movementCommand[key]}`);
+  }
+  console.log("Ctrl + C - move up");
   stdin.on("data", handleUserInput);
   return stdin;
 };
