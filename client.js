2e20ac0913ea4ddc1840c94ee1bbefca8213fa5e
Shree Devi Yadam <shree.yadam@gmail.com>
2021-06-02 17:12:32 +0000

    tried code to move snake

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "135.23.222.131", // IP address here,
     port: 50542 // PORT number here,
@@ -16,8 +16,9 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Connection Established!");
     conn.write("Name: SDY");
-  })
+  });
+
   return conn;
 };
 
-module.exports = {connect};
+module.exports = { connect };
--- play.js
+++ play.js
@@ -2,4 +2,28 @@ const net = require("net");
 const connect = require("./client").connect;
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
+conn.on("connect", () => {
+  // conn.write("Move: up");
+  // setTimeout(() => {
+  //   conn.write("Move: up");
+  //   setTimeout(() => {
+  //     conn.write("Move: up");
+  //     setTimeout(() => {
+  //       conn.write("Move: up");
+  //     }, 500);
+  //     setTimeout(() => {
+  //       conn.write("Move: left");
+  //       setTimeout(() => {
+  //         conn.write("Move: left");
+  //         setTimeout(() => {
+  //           conn.write("Move: left");
+  //           setTimeout(() => {
+  //             conn.write("Move: down");
+  //           }, 500);
+  //         }, 500);
+  //       }, 500);
+  //     }, 500);
+  //   }, 500);
+  // }, 500);
+});
